f8c046a1aa1b41bc3474d917688328172f9d3e07
valentinacastillo02-hash <valentina.castillo02@alumnos.ucn.cl>
2026-05-09 23:04:19 -0400

    Update Gimnasio.java

--- t2_vFinal/src/t2_v2/Gimnasio.java
+++ t2_vFinal/src/t2_v2/Gimnasio.java
@@ -17,9 +17,9 @@ public class Gimnasio {
         this.equipoNombres = eq; 
     }
 
-    public int getNumero() { return numero; } 
-    public String getLider() { return lider; } 
-    public String getEstado() { return estado; } 
+    public int getNumero() { return numero; } //Obtener número del lider del gimnasio.
+    public String getLider() { return lider; } //Obtener el nombre del lider del gimnasio.
+    public String getEstado() { return estado; } //Sber si es que se ha derrotado o no.
     public void setEstado(String e) { this.estado = e; } 
-    public ArrayList<String> getEquipoNombres() { return equipoNombres; } 
+    public ArrayList<String> getEquipoNombres() { return equipoNombres; } //Pokemones del lider del gimnasio.
 }
