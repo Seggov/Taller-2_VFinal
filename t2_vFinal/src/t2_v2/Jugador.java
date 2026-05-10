56275692d227cf454b2274e6084bbcbdb01f4815
valentinacastillo02-hash <valentina.castillo02@alumnos.ucn.cl>
2026-05-09 23:35:11 -0400

    Refactor comments in Jugador class
    Updated comments for clarity and consistency.

--- t2_vFinal/src/t2_v2/Jugador.java
+++ t2_vFinal/src/t2_v2/Jugador.java
@@ -3,31 +3,32 @@ package t2_v2;
 import java.util.ArrayList;
 
 public class Jugador {
+    //Atributos de la clase.
     private String nombre; 
     private String ultimoLider; 
     private ArrayList<Pokemon> pokemons; 
 
-    // constructor del prota
+    // constructor de clase.
     public Jugador(String n, String ul) {
         this.nombre = n; 
         this.ultimoLider = ul; 
         this.pokemons = new ArrayList<>(); 
     }
 
-    // mete bicho al pc
-    public void addPokemon(Pokemon p) {
+   
+    public void addPokemon(Pokemon p) { // Agregar Pokemon al equipo del jugador.
         pokemons.add(p); 
     }
 
     public ArrayList<Pokemon> getPokemons() { return pokemons; }
-    public String getNombre() { return nombre; }
-    public String getUltimoLider() { return ultimoLider; }
+    public String getNombre() { return nombre; }//obtener nombre del Jugador.
+    public String getUltimoLider() { return ultimoLider; } //Obtener nombre del último lider de gimnacio derrotado. 
     public void setUltimoLider(String l) { this.ultimoLider = l; }
 
     // saca los seis principales
     public ArrayList<Pokemon> getEquipo() {
         ArrayList<Pokemon> eq = new ArrayList<>(); 
-        // limite seis bichos
+        // limite seis pokemones.
         for (int i = 0; i < Math.min(6, pokemons.size()); i++) { 
             eq.add(pokemons.get(i)); 
         }
@@ -35,21 +36,21 @@ public class Jugador {
     }
 
     // ve si puede pelear
-    public boolean tieneVivos() {
+    public boolean tieneVivos() {//método para ver si es que se aún quedan pokemones wiwos para pelear. 
         for (Pokemon p : getEquipo()) { 
             if (p.getEstado().equals("Vivo")) return true; 
         }
         return false; 
     }
 
-    // sana todos los bichos
+    // sana todos los pokemones.
     public void curarTodos() {
         for (Pokemon p : pokemons) { 
             p.setEstado("Vivo"); 
         }
     }
     
-    // cambia orden en pc
+    // cambia orden en pc (Opción 3 del menú principal).
     public void intercambiar(int i, int j) {
         Pokemon temp = pokemons.get(i); 
         pokemons.set(i, pokemons.get(j)); 
